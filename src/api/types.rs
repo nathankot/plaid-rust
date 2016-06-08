66e279ccc381e6257e61c85574f1d82831fd4d10
Nathan Kot <me@nathankot.com>
2016-06-07 21:39:57 -0700

    Add full stops to types module docs

--- src/api/types.rs
+++ src/api/types.rs
@@ -1,24 +1,24 @@
 //! Defines a set of type alias to improve understandability.
 
-/// A user's bank account username
+/// A user's bank account username.
 pub type Username = String;
 
-/// A user's bank account password
+/// A user's bank account password.
 pub type Password = String;
 
-/// A user's multi-factor authentication code
+/// A user's multi-factor authentication code.
 pub type MFACode = String;
 
 /// A user's secret access token
 pub type AccessToken = String;
 
-/// Your client id from the Plaid dashboard
+/// Your client id from the Plaid dashboard.
 pub type ClientID = String;
 
-/// Your client secret from the Plaid dashboard
+/// Your client secret from the Plaid dashboard.
 pub type ClientSecret = String;
 
-/// A user's institution. See [here for a list](https://plaid.com/docs/api/#institutions)
+/// A user's institution. See [here for a list](https://plaid.com/docs/api/#institutions).
 pub type Institution = String;
 
 /// Dates are simply stored as their original `String` representation.
