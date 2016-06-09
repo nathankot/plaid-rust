4ac78b8a29ca450828d6101497705fe1517358c3
Nathan Kot <me@nathankot.com>
2016-06-09 00:14:12 -0700

    Update documentation

--- master/implementors/std/error/trait.Error.js
+++ master/implementors/std/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['plaid'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>StdError</a> for <a class='enum' href='plaid/api/error/enum.Error.html' title='plaid::api::error::Error'>Error</a>",];
+implementors['plaid'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>StdError</a> for <a class='enum' href='plaid/api/error/enum.Error.html' title='plaid::api::error::Error'>Error</a>",];implementors['plaid'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>StdError</a> for <a class='enum' href='plaid/api/error/enum.Error.html' title='plaid::api::error::Error'>Error</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
