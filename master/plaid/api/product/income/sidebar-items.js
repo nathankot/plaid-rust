7bf1be30da123a5ff2a930cd3e6c83e75bfe358a
Nathan Kot <me@nathankot.com>
2016-06-09 15:58:37 -0700

    Update documentation

---
+++ master/plaid/api/product/income/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Income","The Income endpoint."],["IncomeData","The underlying data representation of Income."],["IncomeInternalData","Internal data representation of the income response"],["IncomeStream","An income stream represents a stream of income that Plaid has detected from their transactions."]]});
