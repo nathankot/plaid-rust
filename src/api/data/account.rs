c18d7dc638e17efcea679e3f43d8bb0fdc4fc461
Nathan Kot <me@nathankot.com>
2016-06-08 17:57:40 -0700

    Add support for account numbers

--- src/api/data/account.rs
+++ src/api/data/account.rs
@@ -25,14 +25,23 @@ pub struct Account {
     /// This is not always available.
     /// [See here for a list of possible types][sub-types].
     /// [sub-types]: https://plaid.com/docs/api/#connect-account-subtypes
-    pub account_subtype: Option<String>
+    pub account_subtype: Option<String>,
+    /// The user's bank account number.
+    /// Only available when using `api::product::Auth`.
+    pub account_number: Option<String>,
+    /// The user's routing number.
+    /// Only available when using `api::product::Auth`.
+    pub routing_number: Option<String>,
+    /// The user's wire routing number.
+    /// Only available when using `api::product::Auth`.
+    pub wire_routing_number: Option<String>
 }
 
 /// Accounts are decodable from JSON.
 impl Decodable for Account {
 
     fn decode<D: Decoder>(decoder: &mut D) -> Result<Account, D::Error> {
-        decoder.read_struct("root", 6, |decoder| {
+        decoder.read_struct("root", 7, |decoder| {
             let (current_balance, available_balance) =
                 try!(decoder.read_struct_field("balance", 0, |d| {
                     d.read_struct("balance", 2, |d| {
@@ -42,14 +51,32 @@ impl Decodable for Account {
                     })
                 }));
 
+            let (account_number, routing_number, wire_routing_number) =
+                try!(decoder.read_struct_field("numbers", 1, |d| {
+                    d.read_option(|d, exists| {
+                        if exists {
+                            d.read_struct("numbers", 3, |d| {
+                                Ok((try!(d.read_struct_field("account", 0, |d| Decodable::decode(d))),
+                                    try!(d.read_struct_field("routing", 1, |d| Decodable::decode(d))),
+                                    try!(d.read_struct_field("wireRouting", 2, |d| Decodable::decode(d)))))
+                            })
+                        } else {
+                            Ok((None, None, None))
+                        }
+                    })
+                }));
+
             Ok(Account {
-                id: try!(decoder.read_struct_field("_id", 1, |d| Decodable::decode(d))),
-                item_id: try!(decoder.read_struct_field("_item", 2, |d| Decodable::decode(d))),
+                id: try!(decoder.read_struct_field("_id", 2, |d| Decodable::decode(d))),
+                item_id: try!(decoder.read_struct_field("_item", 3, |d| Decodable::decode(d))),
                 current_balance: current_balance,
                 available_balance: available_balance,
-                institution: try!(decoder.read_struct_field("institution_type", 3, |d| Decodable::decode(d))),
-                account_type: try!(decoder.read_struct_field("type", 4, |d| Decodable::decode(d))),
-                account_subtype: try!(decoder.read_struct_field("subtype", 5, |d| Decodable::decode(d)))
+                institution: try!(decoder.read_struct_field("institution_type", 4, |d| Decodable::decode(d))),
+                account_type: try!(decoder.read_struct_field("type", 5, |d| Decodable::decode(d))),
+                account_subtype: try!(decoder.read_struct_field("subtype", 6, |d| Decodable::decode(d))),
+                account_number: account_number,
+                routing_number: routing_number,
+                wire_routing_number: wire_routing_number
             })
         })
     }
@@ -67,20 +94,25 @@ mod tests {
     fn test_decode_wholesome_account() {
         let acc: Account = json::decode(r##"
             {
-            "_id": "YzzrzBrO9OSzo6BXwAvVuL5dmMKMqkhOoEqeo",
-            "_item": "aWWVW4VqGqIdaP495QyOSVLN1nzjLwhXaPDJJ",
-            "_user": "bkkVkMVwQwfYmBMy9jzqHQob9O1KwpFaEyLOE",
-            "balance": {
-                "available": 7205.23,
-                "current": 7255.23
-            },
-            "institution_type": "fake_institution",
-            "meta": {
-                "name": "Plaid Credit Card",
-                "number": "3002"
-            },
-            "type": "depository",
-            "subtype": "checking"
+                "_id": "YzzrzBrO9OSzo6BXwAvVuL5dmMKMqkhOoEqeo",
+                "_item": "aWWVW4VqGqIdaP495QyOSVLN1nzjLwhXaPDJJ",
+                "_user": "bkkVkMVwQwfYmBMy9jzqHQob9O1KwpFaEyLOE",
+                "balance": {
+                    "available": 7205.23,
+                    "current": 7255.23
+                },
+                "institution_type": "fake_institution",
+                "meta": {
+                    "name": "Plaid Credit Card",
+                    "number": "3002"
+                },
+                "type": "depository",
+                "subtype": "checking",
+                "numbers": {
+                    "routing": "021000021",
+                    "account": "9900009606",
+                    "wireRouting": "021000022"
+                }
             }
         "##).unwrap();
 
@@ -91,6 +123,9 @@ mod tests {
         assert_eq!(acc.institution, "fake_institution".to_string());
         assert_eq!(acc.account_type, "depository".to_string());
         assert_eq!(acc.account_subtype, Some("checking".to_string()));
+        assert_eq!(acc.account_number, Some("9900009606".to_string()));
+        assert_eq!(acc.routing_number, Some("021000021".to_string()));
+        assert_eq!(acc.wire_routing_number, Some("021000022".to_string()));
     }
 
     #[test]
@@ -114,6 +149,9 @@ mod tests {
         assert_eq!(acc.institution, "fake_institution".to_string());
         assert_eq!(acc.account_type, "depository".to_string());
         assert_eq!(acc.account_subtype, None);
+        assert_eq!(acc.account_number, None);
+        assert_eq!(acc.routing_number, None);
+        assert_eq!(acc.wire_routing_number, None);
     }
 
 }
